afb2da9a69f32ec90fb63e04f3965b2b9b63aa9e
denervalspelizario <denervalspelizario@gmail.com>
2023-02-05 12:37:36 -0300

    update app instagramClone criacao de header com 2 images clicaveis usando touchableopacity e sua estilizacao

--- App.js
+++ App.js
@@ -1,12 +1,57 @@
 import react from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, FlatList } from 'react-native'; // 1 importando imagem, TouchableOpacity(botão sem design) e flatlist para  lista do feed
 import { Component } from 'react/cjs/react.production.min';
 
-class App extends Component{
+// import das imagens do feed
+import FotoPerfil1 from './assets/image/imageFeed/d1.jpeg';  // importacao de imagem perfil
+import FotoPublicacao1 from './assets/image/imageFeed/d2.jpeg'; // importacao de imagem publicacao
+
+class App extends Component{  
   constructor(props){
     super(props);
     this.state={
-
+      feed : [  // array de objetos com os dados que vão ser usados para simular o feed instagram 
+        {
+          id: '1', 
+          nome: 'Denerval  S Pelizario',
+          imgPerfil: FotoPerfil1,  // uso da foto importada
+          imgPublicacao:  FotoPublicacao1, // uso da foto importada
+          likeada: false,
+          liker: 0
+        },
+        {
+          id: '2', 
+          nome: 'Jessika',
+          imgPerfil: FotoPerfil1,
+          imgPublicacao:  FotoPublicacao1,
+          likeada: false,
+          liker: 0
+        },
+        {
+          id: '3', 
+          nome: 'Gabi Chagas',
+          imgPerfil: FotoPerfil1,
+          imgPublicacao:  FotoPublicacao1,
+          likeada: false,
+          liker: 0
+        },
+        {
+          id: '4', 
+          nome: 'Pedrão',
+          imgPerfil: FotoPerfil1,
+          imgPublicacao:  FotoPublicacao1,
+          likeada: false,
+          liker: 0
+        },
+        {
+          id: '5', 
+          nome: 'Tom',
+          imgPerfil: FotoPerfil1,
+          imgPublicacao:  FotoPublicacao1,
+          likeada: false,
+          liker: 0
+        },
+      ]
     }
   }
   
@@ -16,7 +61,24 @@ class App extends Component{
   render(){
     return(
       <View style={styles.container}>
-        <Text>App Instagram</Text>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Image 
+              source={require('./assets/image/logo.png')}  // importando foto direto da pasta do arquivo usando require
+              style={styles.logo}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Image 
+              source={require('./assets/image/send.png')} // importando foto direto da pasta do arquivo usando require
+              style={styles.send}
+            />
+          </TouchableOpacity>
+          
+
+        </View>
+        
       </View>
     )
   }
@@ -24,9 +86,30 @@ class App extends Component{
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  
+    flex: 1,  // espaço da tela interia
+    paddingVertical: 30, // espaçamento interno vertical
   },
+  header: {
+    height: 55,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignContent: 'center',
+    justifyContent: 'space-between',
+    paddingTop: 10,
+    paddingHorizontal: 10,
+    borderBottomWidth: 0.2,
+    shadowColor: '#000',  // cor da sombra
+    elevation: 1, // cria um nivel acima pra dar sensação de sombra
+
+  },
+  logo: {
+
+  },
+  send: {
+    width: 23,
+    height: 23,
+
+  }
 });
 
 
